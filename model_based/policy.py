22cdd4c3ec017bb05fb7c2ce257d50ba43c528c0
zjh <zhang_jianghu@163.com>
2022-03-21 18:01:03 +0800

    A add some code of pi_evaluate way.

--- model_based/policy.py
+++ model_based/policy.py
@@ -3,22 +3,36 @@
 # Date: 2022/03/20
 
 import gym
+import time
+import re
 import numpy as np
 # import gym.envs.classic_control.grid_mdp as grid_map
 
 
-
 class policy_algorithm:
     def __init__(self, grid_mdp):
-        self.state_and_action_space = grid_mdp.gett()
         self.v = [0, 0, 0, 0, 0, 0, 0, 0]
-        self.pi = {}
+        self.pi = dict()
+        self.pi_space = dict()
+        self.states = grid_mdp.getStates()
+        self.actions = grid_mdp.getActions()
 
-    def pick_action(self, pi):
-        new_pi = dict()
+    # 策略选择函数，类似于K摇臂赌博机
+    def pi_evaluate(self, grid_mdp):
+        for jt in grid_mdp.gett().keys():  # 整理状态-动作空间
+            temp = re.match(r'(.*)_(.*)', jt)
+            if int(temp.group(1)) in self.pi_space.keys():
+                self.pi_space[int(temp.group(1))].append(temp.group(2))
+            else:
+                li = []
+                li.append(temp.group(2))
+                self.pi_space[int(temp.group(1))] = li
 
-
-        return new_pi
+        # for state in self.states:   # 随机初始化策略, 这个不够严谨，换用下面的for
+        #     self.pi[state] = self.actions[np.random.choice(len(self.actions), size=1, replace=True)[0]]
+        for state in self.pi_space.keys():
+            self.pi[state] = self.pi_space[state][np.random.choice(len(self.pi_space[state]),
+                                                                        size=1, replace=True)[0]]
 
     # 评估策略，返回状态值函数
     def policy_evaluate(self, grid_mdp):
@@ -31,7 +45,7 @@ class policy_algorithm:
                 action = self.pi[state]
                 t, s, r = grid_mdp.transform(state, action)
                 new_v = r + grid_mdp.gamma * self.v[s]
-                delta = abs(self.v[state] - new_v)
+                delta = delta + abs(self.v[state] - new_v)
                 self.v[state] = new_v
             if delta < 1e-6: break
 
@@ -55,3 +69,17 @@ class policy_algorithm:
         for i in range(100):
             self.policy_evaluate(grid_mdp)
             self.policy_improve(grid_mdp)
+
+
+if __name__ == "__main__":
+    env = gym.make("GridWorld-v0")
+    env.reset()
+    # env.render()
+
+    DP = policy_algorithm(env)
+    print(DP.states,type(DP.states[0]), DP.actions, type(DP.actions[0]))
+    DP.pi_evaluate(env)
+    print(DP.pi_space)
+    print(DP.pi)
+
+    print("Done!")
